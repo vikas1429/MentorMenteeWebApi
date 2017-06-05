4522bc7ee18ff065f23be801ca26310d00160eee
vikas <vikas1429@gmail.com>
2017-06-06 06:11:51 +1000

    intial commit
    intial commit

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MentorMenteeWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MentorMenteeWebApi.Controllers
+{
+    [RoutePrefix("[controller]/[action]")]
+    public class ValuesController : ApiController
+    {
+        static string sqlstring ="Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|MentorMentee.mdf;Integrated Security = True";
+        
+        
+        // GET api/values
+        [ActionName("GetCategory")]
+        public IEnumerable<string> GetCategory()
+        {
+            SqlConnection sqlcon = new SqlConnection(sqlstring);
+            sqlcon.Open();
+            List<string> val = new List<string>() ;
+            using (SqlCommand cmd = new SqlCommand("SELECT vchCategory from  TBlCategory", sqlcon))
+            {
+                cmd.CommandType = CommandType.Text;
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        val.Add(reader.GetValue(0).ToString());                     
+                    }
+                }
+            }
+            return val;
+           // return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
